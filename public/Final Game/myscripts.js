17a7ffc2ca4e0768fcc7e12bb799d192c3d9caa6
Caleb <Caleb.s.janzen@gmail.com>
2022-05-10 10:54:32 -0700

    Add files via upload

--- public/Final Game/myscripts.js
+++ public/Final Game/myscripts.js
@@ -3,24 +3,44 @@ function load() {
    if(myStorage  != null){
       document.getElementById('myText1').value = myStorage 
     }
-    retrieve()
+    NumHearts()
 }
 
-function load2() {
+function unload() {
    localstorage.setItem('myStoredText',document.getElementById('myText1').value)
+
+}
+
+function retrieve() {
+   document.getElementById('myText1').value = localStorage.getItem('myStoredText')
 }
 
 function store() {
    localStorage.setItem('myStoredText', document.getElementById('myText1').value)
    alert(document.getElementById('myText1').value + ' \nHas been stored')
-   document.getElementById('myText1').value = ""
+   document.getElementById('myText1').value = ''
 }
 
-function retrieve() {
-   document.getElementById('myText1').value = localStorage.getItem('myStoredText')
-   document.getElementById('div01').innerHTML = document.getElementById('myText1').value
+var hearts = localStorage.getItem('myStoredText')
+
+function Hit() {
+   hearts = parseInt(hearts - 1);
+   document.getElementById('myText1').value = hearts
+   store()
+   setTimeout(retrieve, 200)
+   NumHearts()
 }
 
-function clear() {
-   localStorage.clear();
+function NumHearts() {
+   if (hearts == 2) {
+   document.getElementById('heart3').style.left = parseInt(document.getElementById('heart3').style.left) - 1000 + 'px'
+}  if (hearts == 1) {
+   document.getElementById('heart3').style.left = parseInt(document.getElementById('heart3').style.left) - 1000 + 'px'
+   document.getElementById('heart2').style.left = parseInt(document.getElementById('heart2').style.left) - 1000 + 'px'
+} if (hearts <= 0) {
+   document.getElementById('heart3').style.left = parseInt(document.getElementById('heart3').style.left) - 1000 + 'px'
+   document.getElementById('heart2').style.left = parseInt(document.getElementById('heart3').style.left) - 1000 + 'px'
+   document.getElementById('heart1').style.left = parseInt(document.getElementById('heart3').style.left) - 1000 + 'px'
+   alert('You Died')
+}
 }
